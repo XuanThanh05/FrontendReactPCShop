91ae3dd1623673fa16ee622e21770ec94a3f114c
XuanThanh05 <xuanthanhle999@gmail.com>
2026-03-19 13:23:25 +0700

    sửa

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 ﻿import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   const [keyword, setKeyword] = useState('');
--- src/features/homepage/Home.jsx
+++ src/features/homepage/Home.jsx
@@ -1,4 +1,5 @@
 ﻿import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/layout/Header';
 import Footer from '../../components/layout/Footer';
 import LoginPopup from '../../components/layout/LoginPopup';
